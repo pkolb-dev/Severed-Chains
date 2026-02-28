2fd7b13304e1aca5ac736f2728f180468ff417de
Patrick Kolb <64711466+pkolb-dev@users.noreply.github.com>
2026-02-28 14:31:53 -0600

    Shuffled order, added description

--- src/main/java/legend/game/types/MessageBoxType.java
+++ src/main/java/legend/game/types/MessageBoxType.java
@@ -1,11 +1,11 @@
 package legend.game.types;
 
 public enum MessageBoxType {
-  /** This MessageBox has no additional inputs and just displays a message */
+  /** This MessageBox has no additional inputs and just displays a message. Maps to retail type == 0 */
   ALERT,
-  /** This MessageBox displays selectable options, typically yes/no */
-  CONFIRMATION,
-
+  /** This MessageBox is only used for Dabas and is not dismissable, avoid using. Maps to retail type == 1 */
   UNKNOWN,
+  /** This MessageBox displays selectable options, typically yes/no. Maps to retail type == 2 */
+  CONFIRMATION,
   ;
 }
